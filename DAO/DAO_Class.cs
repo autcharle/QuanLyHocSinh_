6da7af15797e662dd1ee7e0992d12debf12e449e
TT <3t.charle@gmail.com>
2021-05-28 17:08:32 +0700

    Tuyen's Completed

--- DAO/DAO_Class.cs
+++ DAO/DAO_Class.cs
@@ -17,7 +17,18 @@ namespace DAO
             using (IDbConnection _dbConnection = _dbContext.CreateConnection())
             {
                 var outputObj = _dbConnection.Query<Class>($"select CLASS_NAME from CLASS where CLASS_ID ='{ID}'").ToList();
+                if (outputObj.Count == 0) return "";
                 var output = outputObj[0].Class_Name;
+                return output;
+            }
+        }
+
+        public List<Class> GetAll()
+        {
+            DBConnect _dbContext = new DBConnect();
+            using (IDbConnection _dbConnection = _dbContext.CreateConnection())
+            {
+                var output = _dbConnection.Query<Class>($"select * from CLASS").ToList();
                 return output;
             }
         }
--- DAO/DAO_Student.cs
+++ DAO/DAO_Student.cs
@@ -13,12 +13,57 @@ namespace DAO
 {
     public class DAO_Student : DBConnect
     {
+        public void InsertAStudent(Student _student)
+        {
+            DBConnect _dbContext = new DBConnect();
+            using (IDbConnection _dbConnection = _dbContext.CreateConnection())
+            {
+                var affectedRows = _dbConnection.Execute($"insert into STUDENT (FULLNAME,GENDER,BIRTHDAY,ADDRESS,EMAIL) values (@FullName,@Gender,@Birthday,@Address,@Email)", new
+                {
+                    FullName = _student.FullName,
+                    Gender = _student.Gender,
+                    Birthday = _student.Birthday,
+                    Address = _student.Address,
+                    Email = _student.Email
+                }) ;
+            }
+        }
         public List<Student> GetAll()
         {
             DBConnect _dbContext = new DBConnect();
             using (IDbConnection _dbConnection = _dbContext.CreateConnection())
             { 
                 var output = _dbConnection.Query<Student>($"select * from STUDENT").ToList();
+                return output;
+            }
+        }
+
+        public List<Student> GetStudentByClassID(int? IDClass)
+        {
+            DBConnect _dbContext = new DBConnect();
+            using (IDbConnection _dbConnection = _dbContext.CreateConnection())
+            {
+                var output = _dbConnection.Query<Student>($"select * from STUDENT where CLASS_ID = '{IDClass}'").ToList();
+                return output;
+            }
+        }
+
+        public List<Student> GetStudentByName(string NameStudent)
+        {
+            DBConnect _dbContext = new DBConnect();
+            using (IDbConnection _dbConnection = _dbContext.CreateConnection())
+            {
+                var output = _dbConnection.Query<Student>($"select * from STUDENT where FULLNAME like N'%{NameStudent}%'").ToList();
+                return output;
+            }
+        }
+
+        public List<Student> GetStudentByNameAndClassID(string NameStudent, int? IDClass)
+        {
+            DBConnect _dbContext = new DBConnect();
+            using (IDbConnection _dbConnection = _dbContext.CreateConnection())
+            {
+                var output = _dbConnection.Query<Student>($"select * from STUDENT where FULLNAME like N'%{NameStudent}%' and CLASS_ID = '{IDClass}'").ToList();
                 return output;
             }
         }
