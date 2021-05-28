abcac31f0ae391ba1d6a1735f6628bed10125801
TT <3t.charle@gmail.com>
2021-05-28 13:08:52 +0700

    Added Class Config

---
+++ DTO/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public partial class Config
+    {
+        public int Config_ID { get; set; }
+        public int? Min_Age { get; set; }
+        public int? Max_Age { get; set; }
+        public double? Subject_Point_Standards { get; set; }
+        public int Max_Ratio { get; set; }
+    }
+}
